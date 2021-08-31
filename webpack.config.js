fdaa76d00191de032368007fbe8f46d34038ee0d
sam bacha <sam@manifoldfinance.com>
2021-08-30 23:18:15 -0700

    cache able

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,21 @@
 'use strict';
 const webpack = require('webpack');
 const path = require('path');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: './lib/lookup.js',
   mode: 'production',
   output: {
-    filename: 'browser.js',
+    filename: 'eth-reveal-[contenthash].js',
     path: path.resolve(__dirname),
     library: 'reveal',
     libraryTarget: 'umd',
     umdNamedDefine: true,
   },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()]
+  },
+  mode: "none"
 };
